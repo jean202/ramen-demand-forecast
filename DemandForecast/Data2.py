5d034fbe22fbdd6d2d92527ce29a71a5dbf82493
jean202 <hb13pud731@gmail.com>
2024-10-14 15:47:48 +0900

    updated Data2.py

--- DemandForecast/Data2.py
+++ DemandForecast/Data2.py
@@ -9,16 +9,27 @@ file_path = '../data2.xlsx'
 # 특정 시트 읽기
 data = pd.read_excel(file_path, sheet_name='2023')
 data_numeric = data.select_dtypes(include='number')
-corr_matrix = data_numeric.corr()
+ramen_data = data[data['대분류'] == '면류.라면류']
+# print("ramen_data", ramen_data)
+# corr_matrix = data_numeric.corr()
 
 # 데이터프레임 출력
 # print(data)
 
-# print(data.info())
-# print(data.shape)
-# print(data.describe())
+# print(ramen_data.info())
+# print(ramen_data.shape)
+# print(ramen_data.describe())
 
-print(data.isnull().sum())
+# print(ramen_data.isnull().sum())
+
+# 결측치 최빈값 처리
+print(ramen_data['소분류'].mode()[0])
+ramen_data['소분류'] = ramen_data['소분류'].fillna(ramen_data['소분류'].mode()[0])
+print(ramen_data.isnull().sum())
+
+print(ramen_data)
+
+# 불필요한 변수 제거.. 뭘 제거해야 하지?
 
 # plt.rc('font', family='AppleGothic')
 # plt.rcParams['axes.unicode_minus'] = False
